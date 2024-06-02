eceda0477a58cd7274e1a157265829ae2926b0bc
impactgalaxy <traderglow@gmail.com>
2024-06-02 14:29:59 +0600

    add pagination

--- src/ALL_CPR/components/apartment/Apartment.jsx
+++ src/ALL_CPR/components/apartment/Apartment.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   ButtonGroup,
   Card,
@@ -10,6 +11,7 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
 export default function Apartment({ apartment }) {
   const {
@@ -22,32 +24,49 @@ export default function Apartment({ apartment }) {
     title,
   } = apartment || {};
   return (
-    <Card maxW="sm">
-      <CardBody>
-        <Image
-          src={image}
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-        />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">{title}</Heading>
-          <Text>{description}</Text>
-          <Text color="blue.600" fontSize="2xl">
-            {rent}
-          </Text>
-        </Stack>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Agreement
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
-          </Button>
-        </ButtonGroup>
-      </CardFooter>
-    </Card>
+    <>
+      <Card maxW="sm">
+        <CardBody>
+          <Image
+            src={image}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+            className="h-80 w-full object-cover"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{title}</Heading>
+            <Text>{description}</Text>
+            <Box>
+              <Text fontWeight="semibold" color="gray.500">
+                Block name: {block_name}
+              </Text>
+              <Text fontWeight="semibold" color="gray.500">
+                Floor number: {floor_no}
+              </Text>
+              <Text fontWeight="semibold" color="gray.500">
+                Apartment number: {apartment_no}
+              </Text>
+            </Box>
+            <Text color="blue.600" fontSize="2xl">
+              $ {rent}
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Agreement
+            </Button>
+            <Button variant="ghost" colorScheme="blue">
+              Add to cart
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
+Apartment.propTypes = {
+  apartment: PropTypes.object,
+};
--- src/ALL_CPR/pages/apartments/Apartments.jsx
+++ src/ALL_CPR/pages/apartments/Apartments.jsx
@@ -1,28 +1,75 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import useAxiosCommon from "../../../others/hooks/axios/useAxiosCommon";
 import Loading from "../../components/shared_components/Loading";
 import Apartment from "../../components/apartment/Apartment";
+import { useState } from "react";
 
 export default function Apartments() {
   const commonApi = useAxiosCommon();
+  const [pageNumber, setPageNumber] = useState(0);
+
   const fetchApartments = async () => {
-    const response = await commonApi.get("/apartments"); // Replace with your endpoint
+    const response = await commonApi.get(
+      `/apartments?pageNumber=${pageNumber}`
+    ); // Replace with your endpoint
     return response.data;
   };
   const { data: apartments, isLoading } = useQuery({
-    queryKey: ["apartment"],
+    queryKey: ["apartment", pageNumber],
     queryFn: fetchApartments,
   });
+
+  const countApartments = async () => {
+    const response = await commonApi.get("/apartmentsCount"); // Replace with your endpoint
+    return response.data;
+  };
+  const { data: apartmentsLength, isLoading: countLoader } = useQuery({
+    queryKey: ["apartmentsCount"],
+    queryFn: countApartments,
+  });
+
+  let totalPage = 0;
+  if (countLoader) {
+    totalPage = 0;
+  } else {
+    totalPage = Math.ceil(apartmentsLength?.result / 6);
+  }
+  const navigationButton = [...Array(totalPage).keys()];
 
   if (isLoading) return <Loading></Loading>;
   return (
     <div>
       <h1>This is apartment house</h1>
-      <div className="container mx-auto p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+      <div className="container mx-auto p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4">
         {apartments.map((apartment) => (
           <Apartment apartment={apartment} key={apartment._id}></Apartment>
         ))}
+      </div>
+      <div className="md:w-1/2 mx-auto p-8 text-center">
+        <div className="join">
+          <button
+            className="join-item btn"
+            disabled={pageNumber === 0}
+            onClick={() => setPageNumber(pageNumber - 1)}>
+            «
+          </button>
+          {navigationButton.map((button, index) => (
+            <button
+              key={index}
+              className={`join-item btn ${
+                pageNumber === button ? "bg-pink-100" : ""
+              }`}
+              onClick={() => setPageNumber(button)}>
+              {button}
+            </button>
+          ))}
+          <button
+            disabled={pageNumber === navigationButton.length - 1}
+            className="join-item btn"
+            onClick={() => setPageNumber(pageNumber + 1)}>
+            »
+          </button>
+        </div>
       </div>
     </div>
   );
